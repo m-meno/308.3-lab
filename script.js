e3cedd2e393fe805064564974d989a582090e624
m-meno <mackenzie.meno@gmail.com>
2025-03-17 14:30:35 -0600

    Part 2: Prime Time

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let i = 1
+// let i = 1
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0){
-        console.log(`Fizz Buzz`);
-    } else if (i % 5 == 0){
-        console.log(`Buzz`);
-    } else if (i % 3 == 0){
-        console.log(`Fizz`);
-    } else (i % 3 !== 0 && i % 5 !== 0);
-        console.log(i)
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0){
+//         console.log(`Fizz Buzz`);
+//     } else if (i % 5 == 0){
+//         console.log(`Buzz`);
+//     } else if (i % 3 == 0){
+//         console.log(`Fizz`);
+//     } else (i % 3 !== 0 && i % 5 !== 0);
+//         console.log(i)
+// }
+
+
+
+for (let n = 5; n < 30; n++){
+    if (n > 5 && n % 2 !== 0 && n % 3 !== 0){
+        console.log(`${n} is a prime number.`); break;
+    }
 }
 
